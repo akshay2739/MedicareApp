a0ef84ea7ee5e2f857751ae85e1b03ced6136087
akshay2739 <akshay2739@gmail.com>
2020-02-21 18:06:10 +0530

    scrollbar updated

--- Navigation/Screens/Patient/Doctor.js
+++ Navigation/Screens/Patient/Doctor.js
@@ -183,7 +183,7 @@ const styles = StyleSheet.create({
     },
     card:{
         backgroundColor:'white',
-        width:300,
+        width:'85%',
         padding:10,
         marginBottom:10,
         justifyContent:'space-around',
